d545c40d0fdb02810064ef7185440d2fe70d51f7
RogerioNascimento-dev <rogerionascimento.dev@outlook.com.br>
2019-09-01 19:17:39 -0300

    implementado icones e novas estilizacões nos botões da modal

--- src/Screens/LevelSelection.js
+++ src/Screens/LevelSelection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, StyleSheet, Text, TouchableOpacity, Modal} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export default props => {
     return (
@@ -10,25 +11,42 @@ export default props => {
             transparent={true}>
             <View style={styles.frame}>
                 <View style={styles.container}>
-                    <Text style={styles.title}>
-                        Selecione o nível de dificuldade
-                    </Text>
+                    <View style={styles.containerTitle}>
+                        <Text style={styles.title}>
+                            Selecione o nível de dificuldade
+                        </Text>
+                    </View>
 
                     <TouchableOpacity
                         style={[styles.button, styles.bgEasy]}
                         onPress={() => props.onLevelSelected(0.1)}>
+                        <Icon
+                            name="sentiment-very-satisfied"
+                            size={30}
+                            color="#FFF"
+                        />
                         <Text style={styles.buttonLabel}>Fácil</Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity
                         style={[styles.button, styles.bgNormal]}
                         onPress={() => props.onLevelSelected(0.2)}>
+                        <Icon
+                            name="sentiment-satisfied"
+                            size={30}
+                            color="#FFF"
+                        />
                         <Text style={styles.buttonLabel}>Intermediário </Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity
                         style={[styles.button, styles.bgHard]}
                         onPress={() => props.onLevelSelected(0.3)}>
+                        <Icon
+                            name="sentiment-very-dissatisfied"
+                            size={30}
+                            color="#FFF"
+                        />
                         <Text style={styles.buttonLabel}>Difícil</Text>
                     </TouchableOpacity>
                 </View>
@@ -48,11 +66,18 @@ const styles = StyleSheet.create({
         backgroundColor: '#EEE',
         alignItems: 'center',
         justifyContent: 'center',
-        padding: 15,
+        paddingBottom: 15,
     },
     title: {
-        fontSize: 25,
-        fontWeight: 'bold',
+        fontSize: 20,
+        //fontWeight: 'bold',
+    },
+    containerTitle: {
+        width: '100%',
+        backgroundColor: '#ccc',
+        padding: 10,
+        borderBottomWidth: 1,
+        borderBottomColor: 'rgba(0,0,0,.5)',
     },
     containerButton: {
         flexDirection: 'row',
@@ -61,12 +86,21 @@ const styles = StyleSheet.create({
     button: {
         marginTop: 10,
         padding: 10,
+        flexDirection: 'row',
         alignItems: 'center',
+        justifyContent: 'center',
+        width: 200,
+        borderWidth: 1,
+        borderLeftColor: '#CCC',
+        borderTopColor: '#CCC',
+        borderRightColor: '#333',
+        borderBottomColor: '#333',
     },
     buttonLabel: {
         fontSize: 20,
         color: '#FFF',
         fontWeight: 'bold',
+        marginLeft: 10,
     },
     bgEasy: {
         backgroundColor: '#46b65d',
